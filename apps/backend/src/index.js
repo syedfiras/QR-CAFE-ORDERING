33b1581aaeface22eef947d74578857a0074509d
Ahmedgannam <ghannama55@gmail.com>
2026-01-10 12:32:12 +0530

    Setup Supabase connection using ES module

--- apps/backend/src/index.js
+++ apps/backend/src/index.js
@@ -1,13 +1,15 @@
-const express = require("express");
-const app = express();
+import express from "express";
+import 'dotenv/config';
+import supabase from "./config/supabase.js";
 
+const app = express();
 app.use(express.json());
 
-app.get("/",(req, res) =>{
-     res.send("Backend is running ");
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
 });
 
 const PORT = 5000;
-app.listen(PORT , () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
