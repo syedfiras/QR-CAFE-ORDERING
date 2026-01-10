c63c60107bc3f5d2d51fed69369a5c80a4ae5e8f
Ahmedgannam <ghannama55@gmail.com>
2026-01-10 12:48:23 +0530

    Added Core backend API's for menu and orders

--- apps/backend/src/index.js
+++ apps/backend/src/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import 'dotenv/config';
 import supabase from "./config/supabase.js";
+import orderRoutes from "./routes/order.routes.js";
+import menuRoutes from "./routes/menu.routes.js";
 
 const app = express();
 app.use(express.json());
-
+app.use("/api/orders", orderRoutes);
+app.use("/api/menu", menuRoutes);
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
